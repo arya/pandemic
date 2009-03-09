36d8236f107d504ec727b7c399ab3f41ca226c90
Arya Asemanfar <arya.asemanfar@gmail.com>
2009-03-08 18:15:02 -0700

    improved mutex counter a tiny bit

--- lib/pandemic/server_side/request.rb
+++ lib/pandemic/server_side/request.rb
@@ -2,21 +2,28 @@ module Pandemic
   module ServerSide
     class Request
       class RequestCounter
-        @@max = (2 ** 30) - 1
+        MAX = (2 ** 30) - 1
         def initialize
           @mutex = Mutex.new
           @counter = 0
+          @resets = 0
         end
-      
+
+        def real_total
+          @mutex.synchronize { (@resets * MAX) + @counter }
+        end
+
         def inc
           @mutex.synchronize do
+            if @counter >= MAX
+              @counter = 0  # to avoid Bignum, it's about 4x slower
+              @resets += 1
+            end
             @counter += 1
-            @counter = 0 if @counter >= @@max # to avoid Bignum, it's about 4x slower
-            return @counter
           end
         end
       end
-    
+          
     
       @@request_count = RequestCounter.new
       attr_reader :body

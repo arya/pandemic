04ddf0301fd4b520b72e0b5b9607622c5606ca00
Arya Asemanfar <arya.asemanfar@gmail.com>
2009-05-26 22:19:32 -0700

    processor tests

---
+++ test/processor_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class HandlerTest < Test::Unit::TestCase
+  include TestHelper
+  
+  context "with a basic echo processor" do
+    setup do
+      echo = Class.new do
+        def process(body)
+          body.reverse
+        end
+      end
+
+      @processor = Pandemic::ServerSide::Processor.new(echo.new)
+    end
+    
+    should "have a running child process" do
+      assert !@processor.closed?
+    end
+    
+    should "close running child" do
+      assert !@processor.closed?
+      @processor.close
+      assert @processor.closed?
+    end
+    
+    should "echo back" do
+      10.times do |i| # just to test the loop part
+        assert_equal "#{i} dlrow olleh", @processor.process("hello world #{i}")
+      end
+    end
+  end
+end
